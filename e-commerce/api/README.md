2ac3007fdd2fea7e06d101ec7bf89ac0b3ae41db
zenjakucreations <rtomarao@gmail.com>
2025-02-07 00:10:15 +0800

    Delete e-commerce directory

--- e-commerce/api/README.md
+++
@@ -1,6 +0,0 @@
-This is a sample e-commerce website, some functions are not working. You can modify this or get an idea for your personal projects. Thanks!
-
-Username: Admin
-Password: Admin!12345
-
-(PHP, Bootstrap, Font Awesome 6)
--- e-commerce/api/browserSync.js
+++
@@ -1,8 +0,0 @@
-const browserSync = require("browser-sync").create();
-
-browserSync.init({
-    proxy: "localhost:3001",
-    files: ["*.php", "pages/*.php", "components/*.php", "css/*.css", "js/*.js"],
-    notify: true,
-    port: 3000,
-});
